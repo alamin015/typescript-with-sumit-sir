3fc50101323e5fb7b065bb1d195b883431aedae1
alamin <alaminsf40@gmail.com>
2023-10-24 11:00:51 +0600

    basic types

---
+++ test.ts
@@ -0,0 +1,9 @@
+let UserName: string;
+let age: number;
+
+// UserName = 98 / there is a type error
+UserName = "hasim amla"; // thats the correcr value assign
+const allAgeArr = [8, 8, 23, 67, 9];
+// allAgeArr.push("hhx");  only number can be pushed into the array
+
+allAgeArr.push(0);
