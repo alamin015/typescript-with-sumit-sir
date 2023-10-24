852e98d8ff4be2c640bd13e9dc86488f5ce233b9
alamin <alaminsf40@gmail.com>
2023-10-24 11:09:52 +0600

    obj basic

--- test.ts
+++ test.ts
@@ -7,3 +7,12 @@ const allAgeArr = [8, 8, 23, 67, 9];
 // allAgeArr.push("hhx");  only number can be pushed into the array
 
 allAgeArr.push(0);
+
+// object
+let myObj = {
+  name: "alamin",
+  age: 12,
+};
+
+// myObj.name = 76 'number' is not assignable to type 'string'
+myObj.name = "hasim"; // thats the correct
